758d1ae89aadb395536754f8047683254e24c134
Pramesh Shrestha <88725877+pramesh-exe@users.noreply.github.com>
2024-04-27 19:30:18 +0545

    project setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/auth/signup", (req, res) => {
+  console.log("signup route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  console.log("login route");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  console.log("logout route");
+});
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
